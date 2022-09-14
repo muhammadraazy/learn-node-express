a2b42ddc6ed02bf80c3d54f610cf4c28a9de08f1
Muhammad Razi <65342312+muhammadraazy@users.noreply.github.com>
2022-09-14 14:45:31 +0700

    added some index
    added some more indexes that mongodb provides

--- mongo-scripts/index.js
+++ mongo-scripts/index.js
@@ -1,3 +1,24 @@
+// working with unique index
+db.restaurants.createIndex(
+   { cuisine: 1, name: 1 },
+   { unique: true }
+)
+
+// sparse index
+// Sparse indexes only contain entries for documents that have the indexed field, even if the index field contains a null value. 
+// The index skips over any document that is missing the indexed field. The index is "sparse" because it does not include all documents of a collection
+db.addresses.createIndex( { "xmpp_id": 1 }, { sparse: true } )
+
+// only index the document that meet the filter condition
+// working with partial index
+db.restaurants.createIndex(
+   { cuisine: 1, name: 1 },
+   { partialFilterExpression: { rating: { $gt: 5 } } }
+)
+
+// query with partial index
+db.restaurants.find( { cuisine: "Italian", rating: { $gte: 8 } } )
+
 // Create index at category in products collection
 db.products.createIndex({
     category: 1
